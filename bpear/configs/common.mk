37b12b85bc503ee8596b697383f9be1b6cad9e1d
Bpear96 <bpear96@gmail.com>
2014-11-14 20:01:06 -0500

    Set prebuilts path.

--- bpear/configs/common.mk
+++ bpear/configs/common.mk
@@ -1,3 +1,6 @@
+# Local path for prebuilts
+LOCAL_PATH:= vendor/bpear/prebuilts/common/system
+
 # Bootanimation support
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/bootanimation.zip:system/media/bootanimation.zip
