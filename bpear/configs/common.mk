394ce806681ffe0a0abb784f0a092c682ceb7870
Brandon <bpear96@gmail.com>
2014-11-16 11:40:11 -0500

    Use prebuilt Google DeskClock

--- bpear/configs/common.mk
+++ bpear/configs/common.mk
@@ -4,3 +4,7 @@ LOCAL_PATH:= vendor/bpear/prebuilts/common/system
 # Bootanimation support
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/bootanimation.zip:system/media/bootanimation.zip
+
+# Use prebuilt Desk Clock
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/app/DeskClockGoogle/DeskClockGoogle.apk:system/app/DeskClockGoogle/DeskClockGoogle.apk
